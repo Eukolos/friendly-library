225d0553f42f4adef78a264d7369c0133af182fc
Eukolos <eminaksoy35@gmail.com>
2022-11-23 23:16:14 +0300

    test container with postgresql

--- library/src/main/java/com/eukolos/library/repository/BookRepository.java
+++ library/src/main/java/com/eukolos/library/repository/BookRepository.java
@@ -2,6 +2,10 @@ package com.eukolos.library.repository;
 
 import com.eukolos.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BookRepository extends JpaRepository<Book,String> {
 }
